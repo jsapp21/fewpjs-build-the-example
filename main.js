a7a5a3d2f88c2c7b02560478d29adc76008498e4
James Sapp <jameseisapp@gmail.com>
2021-02-06 16:30:27 -0600

    commit tree f452e3e2d1a24db2caaeec6c953354221f4ac136

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
+function main(){
+  heartEventListener()
+  addIdsToHearts()
+}
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+
+
+// add eventlistner on the heart
+  // fectch this fuction minicSererCall  
+  // .then
+  // responed to .then using .catch()
+  // display model error remove .hidden class
+  // add a setTimeout to remove model after 5 secs
+
+function addIdsToHearts(){
+  document.body.querySelectorAll('.like-glyph').forEach(heart => heart.id = "heart")
+}
+
+
+function heartEventListener(){
+  document.body.addEventListener('click', function(event){
+    mimicServerCall()
+    .then(function(response){
+      if (event.target.id  === 'heart'){
+        if (event.target.innerText === FULL_HEART){
+          event.target.innerText = EMPTY_HEART
+          event.target.className = ""
+        }
+        else {
+        event.target.innerText = FULL_HEART
+        event.target.className = "activated-heart"
+        }
+      }  
+    })
+    .catch(error => {
+      const div = document.getElementById('modal')
+      div.className = ""
+      setTimeout(function(){ 
+        div.className = "hidden"
+        console.log("this works")}, 5000)
+    }) // this closes .catch
+  }) // this cloes event listner
+} // this closes hearEventListener
 
 
 
@@ -23,3 +65,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+main()
